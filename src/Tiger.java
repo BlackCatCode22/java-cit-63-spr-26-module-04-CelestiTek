cd02e93c43b00565271ae6d792c09fe9cb40c4b5
Jose Arroyo <1047719@MY.SCCCD.EDU>
2026-02-28 13:53:19 -0800

    Tiger class

---
+++ src/Tiger.java
@@ -0,0 +1,31 @@
+public class Tiger extends Animal {
+
+    // Unique features
+    private String stripePattern;   // e.g., "dense", "sparse", "dark"
+    private double territorySize;   // in square miles or km
+
+    // Constructor
+    public Tiger(String name, int age, String species, String stripePattern, double territorySize) {
+        super(name, age, species);
+        this.stripePattern = stripePattern;
+        this.territorySize = territorySize;
+    }
+
+    // Getters
+    public String getStripePattern() {
+        return stripePattern;
+    }
+
+    public double getTerritorySize() {
+        return territorySize;
+    }
+
+    // Setters
+    public void setStripePattern(String stripePattern) {
+        this.stripePattern = stripePattern;
+    }
+
+    public void setTerritorySize(double territorySize) {
+        this.territorySize = territorySize;
+    }
+}
